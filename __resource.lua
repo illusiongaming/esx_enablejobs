6d99fbd5a09acdda6e1023e636901a79367f9561
illusiongaming <36344451+illusiongaming@users.noreply.github.com>
2018-02-10 23:19:55 +0100

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -2,12 +2,12 @@ version '2.0.0'
 
 server_scripts {
 	'config.lua',
-    'server/main.lua'
+    'client.lua'
 
 }
 
 client_scripts {
 	'config.lua',
-    'client/main.lua'	
+    'server.lua'	
 
 }
